74baa6804f63451e16020ae86a829eeafaf6f225
Natalie Cho <nataliecho96@gmail.com>
2023-01-22 10:26:58 -0800

    added function docstring

--- R/project_calories.R
+++ R/project_calories.R
@@ -1,22 +1,29 @@
-#' Calculate the body mass index (BMI) based on current weight and height.
+#' Calculate caloric intake per day based in a target weight. Assumption is
+#' that the goal is losing weight rather than gaining weight.
+#'
+#' @param weight A numeric number of current weight in kilograms (kg).
+#' @param height A numeric number of current height in meters (m).
+#' @param sex A numeric number for sex, used in the Harris–Benedict equation estimation, 
+#' 1 for male and 2 for female 
+#' @param age A numeric number for current age in years.
+#' @param pal A numeric number for physical activity level, can only be one of the four listed values:
+#' 1.2 for "Little/no exercise"
+#' 1.4 for "Light exercise 1-3 times a week"
+#' 1.6 for "Moderate exercise 3-5 times a week"
+#' 1.75 for "Hard exercise 3-5 times a week"
+#' @param target_weight A numeric number for weight goal to be achieved in number_of_days
+#' @param number_of_days A numeric number for number of days allotted to achieve target_weight
+#' @param return_graph TRUE or FALSE on whether to return a graphical visualization instead of a caloric intake value. 
+#'
+#' @return A float or a ggplot visualization. If `return_graph` is `False`, returns a float specifying caloric intake
+#' divided by the number of days. If `return_graph` is `True`, returns a straight line graph of projected 
+#' weight loss per day.
 #' 
-#' Body Mass Index (BMI) is a calculated measure of weight relative to height,
-#' defined as body weight in kilograms divided by height in meters squared
-#' (Keys et al., 1972). If `return_graph` is `TRUE`, an
-#' ggplot object is returned instead of a bmi value.
-#'
-#' @param weight A numeric number of body weight value. 
-#' @param height A numeric number of height value. 
-#' @param return_graph TRUE or FALSE on whether to return a graphical 
-#' visualization instead of a bmi value. 
-#'
-#' @return A numeric number of body mass index value. 
 #' @export
 #'
 #' @examples
-#' calculate_bmi(100, 1.85, return_graph=FALSE)
+#' project_calories(100, 1.85, 1, 25, 1.6, 75, 25, return_graph=False)
+#' 2417.0400000000004
 #' 
-#' 29.218407596785973
-#' 
-calculate_bmi <- function(weight, height, return_graph=FALSE) {
+project_calories <- function(weight, height, sex, age, pal, target_weight, number_of_days, return_graph=FALSE) {
 }
