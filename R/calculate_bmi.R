fde74ccdde7d643eefc2de20608fa2c8fa1e6fcf
hcwang24 <hcwang24@student.ubc.ca>
2023-01-22 01:33:01 -0800

    Creating empty function

---
+++ R/calculate_bmi.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param weight 
+#' @param height 
+#' @param return_graph 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+calculate_bmi <- function(weight, height, return_graph=FALSE) {
+  
+}
