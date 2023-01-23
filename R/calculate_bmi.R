fbc85451e384aaa30491e36f70fe48d135b179f5
hcwang24 <hcwang24@student.ubc.ca>
2023-01-23 09:28:35 -0800

    feat(calculate_bmi.R): adding graph component.

--- R/calculate_bmi.R
+++ R/calculate_bmi.R
@@ -19,9 +19,31 @@
 #' 29.218407596785973
 #' 
 calculate_bmi <- function(weight, height, return_graph=FALSE) {
+  bmi <- weight / (height^2)
   if (return_graph == FALSE) {
-    return(weight / (height^2))
+    return(bmi)
   } else {
-    print("Hi")
+    fig <- plot_ly(
+      type = "indicator",
+      mode = "gauge+number+delta",
+      value = bmi,
+      title = list(text = "Body Mass Index", font = list(size = 24)),
+      delta = list(reference = 25, increasing = list(color = "Pink")),
+      gauge = list(
+        axis = list(range = list(NULL, 50), tickwidth = 1, tickcolor = "black"),
+        bar = list(color = "red"),
+        bgcolor = "white",
+        borderwidth = 2,
+        bordercolor = "black",
+        steps = list(
+          list(range = c(0, 18.5), color = "lightblue"),
+          list(range = c(18.5, 25), color = "lightgreen"),
+          list(range = c(25, 30), color = "lightyellow"),
+          list(range = c(30, 35), color = "orange"),
+          list(range = c(35, 50), color = "Pink"))))
+    fig <- fig %>%
+      layout(
+        font = list(color = "darkblue", family = "Arial"))
+    return(fig)
   }
 }
