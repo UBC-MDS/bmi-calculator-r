793ec8c6882e8e2ab821f2a4d8dfb3c24fe1eec3
HanChen Wang <17226277+hcwang24@users.noreply.github.com>
2023-01-24 22:09:07 -0800

    fix(test-calculate_bmi.R): fixing test description

--- tests/testthat/test-calculate_bmi.R
+++ tests/testthat/test-calculate_bmi.R
@@ -1,17 +1,17 @@
-test_that("function calculates the bmi correctly", {
+test_that("Testing if function calculates the bmi correctly", {
   expect_equal(calculate_bmi(weight=108, height=1.88), 30.56)
   expect_equal(calculate_bmi(weight=50, height=1.65), 18.37)
   expect_equal(calculate_bmi(weight=250, height=1.65), 91.83)
 })
 
-test_that("function returns the bmi graph correctly", {
+test_that("Testing if function returns the bmi graph correctly", {
   fig <- calculate_bmi(weight=108, height=1.88, TRUE)
   expect_s3_class(fig, "htmlwidget")
   expect_false(is.null(fig$x$attrs))
   expect_false(is.null(fig$x$layout))
 })
 
-test_that("function handles exceptions properly", {
+test_that("Testing if function handles exceptions properly", {
   expect_error(calculate_bmi(weight=NULL, height=1.65))
   expect_error(calculate_bmi(weight=100, height=NULL))
   expect_error(calculate_bmi(weight=100, height=1.65, return_graph=1))
@@ -19,7 +19,7 @@ test_that("function handles exceptions properly", {
   expect_error(calculate_bmi())
 })
 
-test_that("function handles negative or zero inputs properly", {
+test_that("Testing if function handles negative or zero inputs properly", {
   expect_error(calculate_bmi(weight=-100, height=1.65))
   expect_error(calculate_bmi(weight=100, height=-1.65))
   expect_error(calculate_bmi(weight=0, height=1.65))
