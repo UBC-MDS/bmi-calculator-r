c863a83c5f136c8ba084953b6449d7c7bc30bc15
HanChen Wang <17226277+hcwang24@users.noreply.github.com>
2023-01-29 00:49:21 -0800

    Revert "fix(tests): edit error messages"

--- tests/testthat/test-project_bmi.R
+++ tests/testthat/test-project_bmi.R
@@ -1,11 +1,11 @@
-test_that("Function does not return the bmi trajectory correctly!", {
+test_that("function returns the bmi trajectory correctly", {
   fig <- project_bmi(108, 1.87, 27, 60, return_graph=TRUE)
   expect_true(class(fig)[2] == "htmlwidget")
   expect_false(is.null(fig$x$attrs))
   expect_false(is.null(fig$x$layout))
 })
 
-test_that("Function does not handle exceptions properly!", {
+test_that("function handles exceptions properly", {
   expect_error(project_bmi(NULL, 1.87, 27, 60, return_graph=TRUE))
   expect_error(project_bmi(108, NULL, 27, 60, return_graph=TRUE))
   expect_error(project_bmi(108, 1.87, NULL, 60, return_graph=TRUE))
@@ -15,7 +15,7 @@ test_that("Function does not handle exceptions properly!", {
   expect_error(project_bmi())
 })
 
-test_that("Function does not handle negative or zero inputs properly!", {
+test_that("function handles negative or zero inputs properly", {
   expect_error(project_bmi(-108, 1.87, 27, 60, return_graph=TRUE))
   expect_error(project_bmi(108, -1.87, 27, 60, return_graph=TRUE))
   expect_error(project_bmi(108, 1.87, 0, 60, return_graph=TRUE))
